7c093922d826006a04b3ebdbc666207f374a4a15
SangyunOck <demibun123@naver.com>
2023-11-12 21:58:09 +0900

    🔥 사용하지 않는 import 삭제

--- src/com/holub/database/Table.java
+++ src/com/holub/database/Table.java
@@ -28,7 +28,6 @@ package com.holub.database;
 
 import java.io.*;
 import java.util.*;
-import com.holub.database.Selector;
 
 /** A table is a database-like table that provides support for
  *  queries.
@@ -244,7 +243,7 @@ public interface Table extends Serializable, Cloneable
 	 */
 	Cursor rows();
 
-	ReadOnlyCursor readOnlyRows();
+	ReadOnlyCursor readOnlyCursor();
 
 	/** Build a representation of the Table using the
 	 *  specified Exporter. Create an object from an
