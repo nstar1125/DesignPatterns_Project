d1e19dbf475a6ffafc5f05c378bc8105566763ed
SonMinGyu <littleking13@naver.com>
2023-11-20 23:57:54 +0900

    🐛TableDistinctVisitor 버그 픽스

--- src/com/holub/database/TableDistinctVisitor.java
+++ src/com/holub/database/TableDistinctVisitor.java
@@ -1,6 +1,7 @@
 package com.holub.database;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,16 +12,25 @@ public class TableDistinctVisitor implements TableVisitor {
     @Override
     public void visit(ConcreteTable table) {
         LinkedList mutRows = table.rows().getRowSet();
+        LinkedList distinctMutRows = new LinkedList();
 
-        List<Object> duplicateMutRows = new ArrayList<>();
+        List<String> stringDistinctMutRows = new ArrayList<>();
 
-        for (int i = 0; i < mutRows.size(); i++) {
-            if (!duplicateMutRows.contains(mutRows.get(i))) {
-                duplicateMutRows.add(mutRows.get(i));
+        Object[][] rows = table.readOnlyCursor().rows();
+        for (int i = 0; i < rows.length; i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int j = 0; j < rows[i].length; j++) {
+                stringBuilder.append(rows[i][j]);
+            }
+
+            if (!stringDistinctMutRows.contains(stringBuilder.toString())) {
+                stringDistinctMutRows.add(stringBuilder.toString());
+                distinctMutRows.add(rows[i]);
             }
         }
+
         mutRows.clear();
-        mutRows.addAll(duplicateMutRows);
+        mutRows.addAll(distinctMutRows);
     }
 
     @Override
