24b00af0478a9633574ff3f523f46b6d94aa5f28
SangyunOck <demibun123@naver.com>
2023-11-25 19:45:20 +0900

    👔 Inspection

---
+++ src/com/team15/erp/model/Inspection.java
@@ -0,0 +1,27 @@
+package com.team15.erp.model;
+
+import com.team15.erp.model.product.ProductFactory;
+import com.team15.erp.scheme.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inspection {
+    public ArrayList<Product> inspect(List<String> products) {
+        ArrayList<Product> productArrayList = new ArrayList<>();
+        for(String product: products) {
+            productArrayList.add(parseProduct(product));
+        }
+
+        return productArrayList;
+    }
+
+    Product parseProduct(String productStr) {
+        String[] token = productStr.split("/");
+
+        String type = token[0].trim();
+        String[] info = token[1].trim().split(" ");
+
+        return new ProductFactory().getProduct(type, info);
+    }
+}
