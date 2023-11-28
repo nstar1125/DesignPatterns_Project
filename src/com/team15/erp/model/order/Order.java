96a05d1e6448f6020b8a53e2fd5c0a0ed77f68f7
SonMinGyu <littleking13@naver.com>
2023-11-28 23:08:21 +0900

    ✨order dto 구현

--- src/com/team15/erp/model/order/Order.java
+++ src/com/team15/erp/model/order/Order.java
@@ -1,34 +1,78 @@
 package com.team15.erp.model.order;
 
+import com.holub.database.ReadOnlyCursor;
+import com.holub.text.ParseFailure;
+import com.team15.erp.dto.order.OrderDto;
+import com.team15.erp.model.Mapper;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class Order {
+public class Order extends Mapper {
 
-    private String id;
-    private String orderNumber;
-    private List<String> orderProductIds;
+    public ArrayList<OrderDto> getAllOrders() throws IOException, ParseFailure {
+        ArrayList<OrderDto> orderDtos = new ArrayList<>();
 
-    public String getId() {
-        return id;
+        this.dbConnection.initialize(DEFAULT_FILE_ROUTE);
+        ReadOnlyCursor order = this.dbConnection.query("select distinct * from order").readOnlyCursor();
+
+        for (Object[] row: order.rows()) {
+            orderDtos.add((OrderDto) map(row, order.columnNames()));
+        }
+
+        return orderDtos;
     }
 
-    public void setId(final String id) {
-        this.id = id;
+    @Override
+    protected Object map(final Object[] row, final String[] columnNames) {
+        Long id = 0L;
+        Long customerId = 0L;
+        ZonedDateTime orderDate = null;
+        List<HashMap<String, Long>> orderProductIds = new ArrayList<>();
+        String orderStatus = null;
+
+        for (int i = 0; i < columnNames.length; i++) {
+            if (columnNames[i].equals("id")) {
+                id = Long.valueOf((String) row[i]);
+            }
+            if (columnNames[i].equals("customer_id")) {
+                customerId = Long.valueOf((String) row[i]);
+            }
+            if (columnNames[i].equals("order_date")) {
+                orderDate = toZonedDateTime((String) row[i]);
+            }
+            if (columnNames[i].equals("order_status")) {
+                orderStatus = (String) row[i];
+            }
+            if (columnNames[i].equals("order_product_ids")) {
+                orderProductIds = toOrderProducts((String) row[i]);
+            }
+        }
+
+        return new OrderDto(
+                id,
+                customerId,
+                orderDate,
+                orderProductIds,
+                orderStatus
+        );
     }
 
-    public String getOrderNumber() {
-        return orderNumber;
-    }
+    private List<HashMap<String, Long>> toOrderProducts(final String inputOrderProductIds) {
+        List<String> parseOrderProducts = new ArrayList<>(List.of(inputOrderProductIds.split("/")));
+        List<HashMap<String, Long>> orderProductIds = new ArrayList<>();
 
-    public void setOrderNumber(final String orderNumber) {
-        this.orderNumber = orderNumber;
-    }
+        for (String parseOrderProduct : parseOrderProducts) {
+            String[] infos = parseOrderProduct.split("-");
+            String productType = infos[0];
+            Long productId = Long.valueOf(infos[1]);
+            HashMap<String, Long> orderProduct = new HashMap<>();
+            orderProduct.put(productType, productId);
+            orderProductIds.add(orderProduct);
+        }
 
-    public List<String> getOrderProductIds() {
         return orderProductIds;
-    }
-
-    public void setOrderProductIds(final List<String> orderProductIds) {
-        this.orderProductIds = orderProductIds;
     }
 }
