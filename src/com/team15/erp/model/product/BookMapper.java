48bb8574b8a73c289dc64f98e8e6419cd4b79271
seunguri <safg0421@gmail.com>
2023-11-29 20:36:16 +0900

    :sparkles: Shoes/BookMapper selectBy() - Entry에 기타 계산 추가 - Dbase에 datafile 추가

--- src/com/team15/erp/model/product/BookMapper.java
+++ src/com/team15/erp/model/product/BookMapper.java
@@ -1,16 +1,31 @@
 package com.team15.erp.model.product;
 
+import com.holub.database.ReadOnlyCursor;
+import com.holub.text.ParseFailure;
 import com.team15.erp.model.Mapper;
 import com.team15.erp.entity.product.Book;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 public class BookMapper extends Mapper<Book> {
+
+    public List<Book> selectByNameWriter(String productName, String writer) throws IOException, ParseFailure {
+        ReadOnlyCursor cursor = dbConnection.query("select * from book where product_name = \""+productName+"\"").readOnlyCursor();
+
+        return Arrays.stream(cursor.rows())
+                .map(row -> map(row, cursor.columnNames()))
+                .toList();
+    }
+
     @Override
     protected Book map(final Object[] row, final String[] columnNames) {
         Book book = new Book();
         for (int i = 0; i < columnNames.length; i++) {
             switch (columnNames[i]) {
                 case "id":
-                    book.setId((Long) row[i]);
+                    book.setId(Long.valueOf((String) row[i]));
                     break;
                 case "product_name":
                     book.setProductName((String) row[i]);
--- src/com/team15/erp/model/product/ShoesMapper.java
+++ src/com/team15/erp/model/product/ShoesMapper.java
@@ -1,16 +1,32 @@
 package com.team15.erp.model.product;
 
+import com.holub.database.ReadOnlyCursor;
+import com.holub.text.ParseFailure;
 import com.team15.erp.model.Mapper;
 import com.team15.erp.entity.product.Shoes;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ShoesMapper extends Mapper<Shoes> {
+
+    public List<Shoes> selectByNameTypeBrandSize(String productName, String brand, Integer size) throws IOException, ParseFailure {
+        ReadOnlyCursor cursor = dbConnection.query("select * from shoes where product_name = \""+productName+"\"").readOnlyCursor();
+
+        return Arrays.stream(cursor.rows())
+                .map(row -> map(row, cursor.columnNames()))
+                .collect(Collectors.toList());
+    }
+
     @Override
     protected Shoes map(final Object[] row, final String[] columnNames) {
         Shoes shoes = new Shoes();
         for (int i = 0; i < columnNames.length; i++) {
             switch (columnNames[i]) {
                 case "id":
-                    shoes.setId((Long) row[i]);
+                    shoes.setId(Long.valueOf((String)row[i]));
                     break;
                 case "product_name":
                     shoes.setProductName((String) row[i]);
